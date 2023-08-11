12f4d658d6641b3ed7bff7d039beb4fbe106d713
Denis Karnafel <denisk9999@mail.ru>
2023-08-11 16:29:54 +0700

    update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,7 +6,10 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
+  .version('1.0.0')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format <type>', 'output format')
 
 program.parse();
 
