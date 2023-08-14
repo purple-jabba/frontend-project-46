e246749781a171f64f522ff4ed16062e8c158a9a
Denis Karnafel <denisk9999@mail.ru>
2023-08-14 18:40:55 +0700

    update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import getDiffJson from '../src/getDiffJson.js';
 
 const program = new Command();
 
@@ -8,8 +9,10 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .argument('<filepath1>')
-  .argument('<filepath2>')
-  .option('-f, --format <type>', 'output format');
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => {
+    getDiffJson(filepath1, filepath2);
+  });
 
 program.parse();
