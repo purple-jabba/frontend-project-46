afb5c39e9a0dea4f19d649685fd9469ede903008
Denis Karnafel <denisk9999@mail.ru>
2023-09-02 21:56:23 +0700

    update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import getDiffJson from '../src/getDiffJson.js';
+import gendiff from '../src/index.js';
 
 const program = new Command();
 
@@ -12,7 +12,7 @@ program
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format')
   .action((filepath1, filepath2) => {
-    getDiffJson(filepath1, filepath2);
+    gendiff(filepath1, filepath2);
   });
 
 program.parse();
