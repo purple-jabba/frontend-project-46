f23b4723bc5180c241a10c174d0e471541a92d05
Denis Karnafel <denisk9999@mail.ru>
2023-09-07 20:26:14 +0700

    add test for json format

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,6 +4,7 @@ import readFile from '../src/utils/readFile.js';
 
 const resultStylish = readFile('./__fixtures__/toBeExpectedStylish.ini');
 const resultPlain = readFile('./__fixtures__/toBeExpectedPlain.ini');
+const resultJson = readFile('./__fixtures__/toBeExpectedJson.ini');
 
 test('stylish output check', () => {
   const filepath1 = '__fixtures__/file1.yml';
@@ -21,4 +22,13 @@ test('plain output check', () => {
   const output = gendiff(filepath1, filepath2, 'plain');
 
   expect(output).toEqual(resultPlain);
+});
+
+test('json output check', () => {
+  const filepath1 = '__fixtures__/file1.yml';
+  const filepath2 = '__fixtures__/file2.json';
+
+  const output = gendiff(filepath1, filepath2, 'json');
+
+  expect(output).toEqual(resultJson);
 });
