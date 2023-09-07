85441857c5f80e90748228bbcc50e643bebf776b
Denis Karnafel <denisk9999@mail.ru>
2023-09-07 19:29:20 +0700

    add plain.js

---
+++ src/formatters/plain.js
@@ -0,0 +1,33 @@
+import path from 'path';
+import _ from 'lodash';
+
+const plainValue = (node) => {
+  if (_.isObject(node)) {
+    return '[complex value]';
+  }
+  if (typeof node === 'string') {
+    return `'${node}'`;
+  }
+  return String(node);
+};
+
+const displayPlain = (node, keypath = '') => {
+  const filterUnchangedType = node.filter((data) => data.type !== 'unchanged');
+  const result = filterUnchangedType.map((data) => {
+    const collectedKeys = path.join(keypath, data.key).replace('/', '.');
+    switch (data.type) {
+      case 'nested':
+        return displayPlain(data.children, collectedKeys);
+      case 'added':
+        return `Property '${collectedKeys}' was added with value: ${plainValue(data.value)}`;
+      case 'deleted':
+        return `Property '${collectedKeys}' was removed`;
+      case 'changed':
+        return `Property '${collectedKeys}' was updated. From ${plainValue(data.initialValue)} to ${plainValue(data.changedValue)}`;
+      default: throw new Error(`Wrong type of data ${data.type}.`);
+    }
+  });
+  return result.join('\n');
+};
+
+export default displayPlain;
