f3559943c1998d57f5c86dd44ff81ffd2245031f
Denis Karnafel <denisk9999@mail.ru>
2023-09-11 16:38:50 +0700

    update index.js and parsers.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
+import { cwd } from 'node:process';
+import { readFileSync } from 'node:fs';
+import { resolve, extname } from 'node:path';
 import getDifference from './getDifference.js';
-import parse from './utils/parsers.js';
+import parse from './parsers.js';
 import selectFormat from './formatters/index.js';
 
+const buildFullPath = (filepath) => resolve(cwd(), filepath);
+
+const readFile = (path) => readFileSync(buildFullPath(path), 'utf8');
+
 const gendiff = (filepath1, filepath2, format = 'stylish') => {
-  const path1ToParse = parse(filepath1);
-  const path2ToParse = parse(filepath2);
+  const path1ToParse = parse(readFile(filepath1), extname(filepath1).split('.')[1]);
+  const path2ToParse = parse(readFile(filepath2), extname(filepath2).split('.')[1]);
 
   const copyOfPath1 = path1ToParse;
   const copyOfPath2 = path2ToParse;
--- src/utils/readFile.js
+++
@@ -1,6 +0,0 @@
-import { readFileSync } from 'node:fs';
-import convertPath from './convertPath.js';
-
-const readFile = (path) => readFileSync(convertPath(path), 'utf8');
-
-export default readFile;
