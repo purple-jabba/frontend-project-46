df1437266060976d96e58fedc72c12327f690833
Denis Karnafel <denisk9999@mail.ru>
2023-09-11 17:39:02 +0700

    update plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,7 +1,7 @@
 import path from 'path';
 import _ from 'lodash';
 
-const plainValue = (node) => {
+const stringify = (node) => {
   if (_.isObject(node)) {
     return '[complex value]';
   }
@@ -12,21 +12,24 @@ const plainValue = (node) => {
 };
 
 const displayPlain = (node, keypath = '') => {
-  const filterUnchangedType = node.filter((data) => data.type !== 'unchanged');
-  const result = filterUnchangedType.map((data) => {
-    const collectedKeys = path.join(keypath, data.key).replace('/', '.');
-    switch (data.type) {
-      case 'nested':
-        return displayPlain(data.children, collectedKeys);
-      case 'added':
-        return `Property '${collectedKeys}' was added with value: ${plainValue(data.value)}`;
-      case 'deleted':
-        return `Property '${collectedKeys}' was removed`;
-      case 'changed':
-        return `Property '${collectedKeys}' was updated. From ${plainValue(data.value1)} to ${plainValue(data.value2)}`;
-      default: throw new Error(`Wrong type of data ${data.type}.`);
-    }
-  });
+  const result = node
+    .map((data) => {
+      const collectedKeys = path.join(keypath, data.key).replace('/', '.');
+      switch (data.type) {
+        case 'nested':
+          return displayPlain(data.children, collectedKeys);
+        case 'added':
+          return `Property '${collectedKeys}' was added with value: ${stringify(data.value)}`;
+        case 'deleted':
+          return `Property '${collectedKeys}' was removed`;
+        case 'changed':
+          return `Property '${collectedKeys}' was updated. From ${stringify(data.value1)} to ${stringify(data.value2)}`;
+        case 'unchanged':
+          return null;
+        default: throw new Error(`Wrong type of data ${data.type}.`);
+      }
+    })
+    .filter(Boolean);
   return result.join('\n');
 };
 
