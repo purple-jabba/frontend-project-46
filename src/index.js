b838234f910c09e681e8ac7da50bfd4c1cfc48cb
Denis Karnafel <denisk9999@mail.ru>
2023-09-11 18:04:19 +0700

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import { readFileSync } from 'node:fs';
 import { resolve, extname } from 'node:path';
 import getDifference from './getDifference.js';
 import parse from './parsers.js';
-import selectFormat from './formatters/index.js';
+import format from './formatters/index.js';
 
 const buildFullPath = (filepath) => resolve(cwd(), filepath);
 
 const readFile = (path) => readFileSync(buildFullPath(path), 'utf8');
 
-const gendiff = (filepath1, filepath2, format = 'stylish') => {
+const gendiff = (filepath1, filepath2, outputFormat = 'stylish') => {
   const path1ToParse = parse(readFile(filepath1), extname(filepath1).split('.')[1]);
   const path2ToParse = parse(readFile(filepath2), extname(filepath2).split('.')[1]);
 
   const compare = getDifference(path1ToParse, path2ToParse);
-  const displayDifference = selectFormat(compare, format);
+  const displayDifference = format(compare, outputFormat);
   return displayDifference;
 };
 
