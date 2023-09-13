93b5560dfc95f102c096a62d4d9e7bfd1b79117c
Denis Karnafel <denisk9999@mail.ru>
2023-09-13 12:40:45 +0700

    update index.js

--- src/index.js
+++ src/index.js
@@ -9,11 +9,13 @@ const buildFullPath = (filepath) => resolve(cwd(), filepath);
 
 const readFile = (path) => readFileSync(buildFullPath(path), 'utf8');
 
-const gendiff = (filepath1, filepath2, outputFormat = 'stylish') => {
-  const path1ToParse = parse(readFile(filepath1), extname(filepath1).split('.')[1]);
-  const path2ToParse = parse(readFile(filepath2), extname(filepath2).split('.')[1]);
+const getExtension = (filepath) => extname(filepath).split('.')[1];
 
-  const compare = getDifference(path1ToParse, path2ToParse);
+const gendiff = (filepath1, filepath2, outputFormat = 'stylish') => {
+  const parseFilepath1 = parse(readFile(filepath1), getExtension(filepath1));
+  const parseFilepath2 = parse(readFile(filepath2), getExtension(filepath2));
+
+  const compare = getDifference(parseFilepath1, parseFilepath2);
   const displayDifference = format(compare, outputFormat);
   return displayDifference;
 };
