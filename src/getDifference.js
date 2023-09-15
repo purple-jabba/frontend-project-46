c34a2683be4b5a24d5644dc413122f8b72e785fd
Denis Karnafel <denisk9999@mail.ru>
2023-09-15 16:35:13 +0700

    update getDifference.js

--- src/getDifference.js
+++ src/getDifference.js
@@ -1,24 +1,24 @@
 import _ from 'lodash';
 
-const getDifference = (filename1, filename2) => {
-  const sortedKeys = _.sortBy(_.union(Object.keys(filename1), Object.keys(filename2)));
+const getDifference = (data1, data2) => {
+  const sortedKeys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
 
   const result = sortedKeys.map((key) => {
-    if (_.isPlainObject(filename1[key]) && _.isPlainObject(filename2[key])) {
-      return { type: 'nested', key, children: getDifference(filename1[key], filename2[key]) };
+    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
+      return { type: 'nested', key, children: getDifference(data1[key], data2[key]) };
     }
-    if (!Object.hasOwn(filename1, key)) {
-      return { type: 'added', key, value: filename2[key] };
+    if (!Object.hasOwn(data1, key)) {
+      return { type: 'added', key, value: data2[key] };
     }
-    if (!Object.hasOwn(filename2, key)) {
-      return { type: 'deleted', key, value: filename1[key] };
+    if (!Object.hasOwn(data2, key)) {
+      return { type: 'deleted', key, value: data1[key] };
     }
-    if (!_.isEqual(filename1[key], filename2[key])) {
+    if (!_.isEqual(data1[key], data2[key])) {
       return {
-        type: 'changed', key, value1: filename1[key], value2: filename2[key],
+        type: 'changed', key, value1: data1[key], value2: data2[key],
       };
     }
-    return { type: 'unchanged', key, value: filename1[key] };
+    return { type: 'unchanged', key, value: data1[key] };
   });
   return result;
 };
